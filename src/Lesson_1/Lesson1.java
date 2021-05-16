c88142b48133e4b8aef7b5b630fc2d74469fd3e3
Viktor Vasiliev <vitavas67@mail.ru>
2021-05-17 02:26:54 +0300

    Homework

--- src/Lesson_1/Lesson1.java
+++ src/Lesson_1/Lesson1.java
@@ -3,5 +3,19 @@ package Lesson_1;
 public class Lesson1 {
     public static void main(String[] args) {
         System.out.println("Home Work");
+//        Создать переменные всех пройденных типов данных, и инициализировать их значения;
+        byte b1 = -3;
+        short s1 = 2022;
+        int i1 = 45655;
+        long l1 = 84848444L;
+        float f1 = 78.45f;
+        double d1 = -789.546;
+        char c1 = 'f';
+        boolean b2 = true;
+        String str = "Ok";
+
+//        Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+//        где a, b, c, d – входные параметры этого метода;
+        System.out.println("1");
     }
 }
