ce44ecc2dceb1e383549fafb651cfc46cce2c564
Viktor Vasiliev <vitavas67@mail.ru>
2021-05-23 16:00:38 +0300

    Homework_2

---
+++ src/Lesson_3/Lesson_3.java
@@ -0,0 +1,16 @@
+package Lesson_3;
+
+//import java.util.Scanner;
+//
+//public class Lesson_3 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        String b = sc.nextLine();
+//        String c = sc.next();
+//
+//
+//
+//
+//    }
+//}
