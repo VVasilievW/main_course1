7e33a55870f0aa975d4941811f78a33c41fa9b83
Viktor Vasiliev <vitavas67@mail.ru>
2021-05-23 16:19:00 +0300

    Homework_2.1

--- src/Lesson_2/Lesson_2.java
+++ src/Lesson_2/Lesson_2.java
@@ -1,4 +1,8 @@
 package Lesson_2;
 
 public class Lesson_2 {
+    public static void main(String[] args) {
+        System.out.println("DZ_2");
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My Lesson3");
+        System.out.println("My Lesson2");
 
 
     }
