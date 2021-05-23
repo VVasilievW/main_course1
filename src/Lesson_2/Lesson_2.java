b73caffe9eda4b4a60534f0482fca6fd65c84158
Viktor Vasiliev <vitavas67@mail.ru>
2021-05-23 16:45:16 +0300

    Homework_2.1

--- src/Lesson_2/Lesson_2.java
+++ src/Lesson_2/Lesson_2.java
@@ -3,6 +3,8 @@ package Lesson_2;
 public class Lesson_2 {
     public static void main(String[] args) {
         System.out.println("DZ_2");
+        //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
 
     }
 }
